0f67ed923e94f996d534f37d6ce2c61a81487971
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-03-20 15:46:38 +0100

    v1.0

---
+++ comment-form-autofill.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Plugin Name: Comment form autofill
+ * Description: Autofill comment form fields by URL parameters.
+ * Version:     1.0
+ * Author:      Jory Hogeveen
+ * Author URI:  https://www.keraweb.nl
+ */
+
+if ( ! defined ( 'ABSPATH' ) ) {
+	die;
+}
+
+Comment_Form_Autofill::get_instance();
+
+class Comment_Form_Autofill
+{
+	/**
+	 * @var Comment_Form_Autofill
+	 */
+	private static $_instance = null;
+
+	/**
+	 * @return Comment_Form_Autofill
+	 */
+	public static function get_instance() {
+		if ( ! self::$_instance ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * Comment_Form_Autofill constructor.
+	 */
+	protected function __construct() {
+		add_action( 'wp_footer', __CLASS__ . '::script' );
+	}
+
+	/**
+	 * Render inline script.
+	 */
+	public static function script() {
+		if ( ! did_action( 'comment_form' ) ) {
+			return;
+		}
+
+		echo '<script id="keraweb-comment-form-autofill">';
+		include 'js/inline-script.js';
+		echo '</script>';
+	}
+
+}
---
+++ js/inline-script.js
@@ -0,0 +1,78 @@
+jQuery( function( $ ) {
+	var url_vars = [];
+
+	window.location.search.replace(
+		/[?&]+([^=&]+)=([^&]*)/gi,
+		function( m, key, value ) {
+			url_vars[ key ] = value;
+		}
+	);
+
+	$( '.comment-form' ).each( function() {
+		var $this  = $( this ),
+			$input    = $this.find( 'input' ),
+			$select   = $this.find( 'select' ),
+			$textarea = $this.find( 'textarea' );
+
+		$input.each( function() {
+			var $this     = $( this ),
+				type      = $this.attr( 'type' ),
+				name      = $this.attr( 'name' ),
+				url_value = getUrlParam( name ),
+				value;
+
+			// No url value found.
+			if ( null === url_value ) {
+				return true;
+			}
+
+			switch ( type ) {
+				case 'radio':
+					value = $this.attr( 'value' );
+					$this.prop( 'checked', ( value === url_value ) );
+					break;
+				case 'checkbox':
+					$this.prop( 'checked', Boolean( url_value ) );
+					break;
+				default:
+					value = $this.val();
+					if ( ! value ) {
+						$this.val( url_value );
+					}
+					break;
+			}
+		} );
+
+		$select.each( function () {
+			var $this = $( this ),
+				name  = $this.attr( 'name' ),
+				value = $this.val();
+			if ( ! value ) {
+				value = getUrlParam( name );
+				if ( value ) {
+					console.log( value );
+					$this.val( value );
+				}
+			}
+		} );
+
+		$textarea.each( function () {
+			var $this = $( this ),
+				name  = $this.attr( 'name' ),
+				value = $this.val();
+			if ( ! value ) {
+				$this.val( getUrlParam( name, value ) );
+			}
+		} );
+	} );
+
+	function getUrlParam( parameter, default_value ) {
+		if ( 'undefined' !== typeof url_vars[ parameter ] ) {
+			return decodeURIComponent( url_vars[ parameter ] );
+		}
+		if ( default_value ) {
+			return default_value;
+		}
+		return null;
+	}
+} );
